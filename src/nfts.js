a8ef9cec1832197256e934df23451ed342650118
bangjo321 <160465108+bangjo321@users.noreply.github.com>
2024-02-19 14:39:48 +0700

    Add files via upload

---
+++ src/nfts.js
@@ -0,0 +1,47 @@
+import Image2 from './assets/blink-lady.gif';
+import Image8 from './assets/guy-smoking.gif';
+import Image11 from './assets/rotate-skeletons.gif';
+import Image14 from './assets/walking-duck.gif';
+
+
+export const nfts = [
+
+{
+    title: "Blinking Bitch",
+    artist: "John Christotle",
+    year: "2006",
+    movieID: "00002",
+    type: "NFT GIF",
+    image: Image2,
+    rating: 5,
+},
+{
+    title: "Smoky-Bado",
+    artist: "John Christotle",
+    year: "2006",
+    movieID: "00008",
+    type: "NFT GIF",
+    image: Image8,
+    rating: 5,
+},
+{
+    title: "Rotate-Skeletons",
+    artist: "John Christotle",
+    year: "2006",
+    movieID: "00011",
+    type: "NFT GIF",
+    image: Image11,
+    rating: 4,
+},
+{
+    title: "Walking Duck",
+    artist: "John Christotle",
+    year: "2006",
+    movieID: "00014",
+    type: "NFT GIF",
+    image: Image14,
+    rating: 5,
+},
+
+
+]
